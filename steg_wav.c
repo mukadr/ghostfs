31ecc0b85535dd18cedbd9f0aae5b0c019280b6c
Mauro Dreissig <mukadr@gmail.com>
2014-11-25 15:57:46 -0200

    steg_wav: Scan wav file sections

--- steg_wav.c
+++ steg_wav.c
@@ -1,9 +1,15 @@
 #include <err.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 #include "steg_wav.h"
 
 struct wav {
+	int bps; // bits per sample
+	unsigned char *data; // data section
+	size_t len; // data section len
 	struct steg steg;
 };
 
@@ -36,6 +42,47 @@ static const struct steg_ops wav_ops = {
 /* Parses the wav file to locate its data section */
 static int wav_init(struct wav *wav)
 {
+	unsigned char *p = wav->steg.map;
+	size_t len = wav->steg.len;
+	uint16_t audio_fmt;
+
+	while (len >= 4) {
+		if (memcmp(p, "fmt ", 4) == 0)
+			break;
+		p++;
+		len--;
+	}
+	if (len < 24) {
+		warnx("wav: incomplete or no 'fmt ' section found");
+		return -1;
+	}
+	audio_fmt = *(uint16_t *)(p + 8);
+	if (audio_fmt != 1) {
+		warnx("wav: only PCM format supported");
+		return -1;
+	}
+	wav->bps = *(uint16_t *)(p + 22);
+
+	while (len >= 4) {
+		if (memcmp(p, "data ", 4) == 0)
+			break;
+		p++;
+		len--;
+	}
+	if (len < 8) {
+		warnx("wav: incomplete or no 'data' section found");
+		return -1;
+	}
+	wav->data = p + 8;
+	wav->len = *(uint32_t *)(p + 4);
+
+	if (wav->len > wav->steg.len) {
+		warnx("wav: bad data section");
+		return -1;
+	}
+
+	printf("bps = %d\n", wav->bps);
+	printf("data size = %lu\n", wav->len);
 	return 0;
 }
 
